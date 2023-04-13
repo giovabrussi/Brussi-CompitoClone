8825c0953c90f103da5905260c4a30762c314ea6
brussi <brussi.giovanni@itismeucci.com>
2023-04-13 08:22:16 +0200

    inserito nome

--- stampa.cpp
+++ stampa.cpp
@@ -1,7 +1,7 @@
 /*
 Scrivere un programma che stampi "n" volte la stringa "Bada come la fuma"  con "n" dato in input dall'utente.
 
-NOME COGNOME DATA
+Giovanni Brussi 13/04/2023
 */
 
 # include <iostream>
